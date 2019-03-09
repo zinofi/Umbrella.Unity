4cdabd32f17fd743414c4caa85644d240daa63b6
Richard Edwards <richard.edwards@zinofi.co.uk>
2019-03-09 19:48:28 +0000

    Initial commit of the code move across from the main Umbrella repo.

---
+++ src/Umbrella.Unity/Utilities/Validation/IUnityValidationUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static Umbrella.Unity.Utilities.Validation.UnityValidationUtility;
+
+namespace Umbrella.Unity.Utilities.Validation
+{
+    public interface IUnityValidationUtility
+    {
+        UnityValidationResult TryValidateModel<TModel>(TModel model);
+        void ValidateModel<TModel>(TModel model);
+    }
+}
